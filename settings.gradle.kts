e92b8fb773e8bf674b413dc6ac0ce96996d1cd2d
IGBerko <helpers@uncoree.org>
2026-06-27 13:48:41 +0300

    Initial commit (uncoree)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,13 +15,13 @@ if (!file(".git").exists()) {
     val errorText = """
         
         =====================[ ERROR ]=====================
-         The Paper project directory is not a properly cloned Git repository.
+         The Uncoree  project directory is not a properly cloned Git repository.
          
-         In order to build Paper from source you must clone
+         In order to build Uncoree from source you must clone
          the Paper repository using Git, not download a code
          zip from GitHub.
          
-         Built Paper jars are available for download at
+         Built Uncoree jars are available for download at
          https://papermc.io/downloads/paper
          
          See https://github.com/PaperMC/Paper/blob/master/CONTRIBUTING.md
